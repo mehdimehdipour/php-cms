9fdc0de0db26265e2c5fc8ef20acd4cffbc1bb5e
mehdimehdipour <mehdimehdipour21@gmail.com>
2024-05-24 22:11:17 +0330

    Added header and footer pages.

---
+++ about.php
@@ -0,0 +1,26 @@
+<?php include_once 'header.php'?>
+<br>
+<br>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+
+
+            <div class="post-single">
+                <div class="post-title-single"><h1>درباره ما</h1></div>
+                <div class="clearfix"></div>
+                <div class="thumb-single-product"><img src="img/default.jpg" class="attachment-medium size-medium wp-post-image" alt=""></div>
+
+                <div class="post-txt-single">
+                    <p>وبسافت۳ فعالیت خود را در تاریخ ۱۳۹۳/۱۰/۷ در زمینه تولید فیلم های آموزش برنامه نویسی و طراحی سایت و مقالات آموزشی آغاز کرده.<br>
+                        یکی از دغدغه های بیشتر افراد محصل در رشته های مختلف و به خصوص رشته کامپیوتر، ترس از بیکاری و بدست نیاوردن شغل مورد علاقشون بعد از فارق التحصیلی هست. ما با در نظر گرفتن این موضوع و تلاش برای پوشش دادن این نوع دغدغه ها در حد توان به عنوان یک مرجع آموزشی سعی میکنیم با آموزش دادن و پرورش متخصص برای بازارکار در حوضه برنامه نویسی و طراحی سایت به علاقه مندان کمک کنیم تا از این راه به اهداف شغلی خود دست پیدا کنند.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+    <br>
+    <br>
+<?php include_once 'footer.php';?>
---
+++ footer.php
@@ -0,0 +1,53 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 box-footer">
+                <h5>دسترسی سریع</h5>
+                <div class="top-bar">
+                    <div class="right-top-bar">
+                        <ul>
+                            <li id="menu-item-69" class="menu-item"><a title="وبسافت3" href="http://websoft3.com"
+                                                                       dideo-checked="true">صفحه اصلی</a></li>
+                            <li class="menu-item"><a href="#">درباره ما</a></li>
+                            <li class="menu-item"><a href="#">تماس با ما</a></li>
+                            <li class="menu-item"><a href="#">پرداخت آنلاین</a></li>
+                            <li class="menu-item"><a href="#">حساب کاربری من</a></li>
+                            <li class="menu-item"><a href="#">همکاری با ما</a></li>
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
+            <div class="col-md-8 box-footer">
+                <h5>درباره وبسافت3</h5>
+                <div class="top-bar">
+                    <div class="right-top-bar">
+                        <div class="textwidget">وبسافت3 فعالیت خود را در تاریخ 1393/10/7 در زمینه تولید فیلم های آموزش
+                            طراحی سایت و و وردپرس آغاز کرده.
+                            یکی از دغدغه های بیشتر افراد محصل در رشته های مختلف و به خصوص رشته کامپیوتر، ترس از بیکاری و
+                            بدست نیاوردن شغل مورد علاقه شان بعد از فارق التحصیلی هست. ما با در نظر گرفتن این موضوع و
+                            تلاش برای پوشش دادن این نوع دغدغه ها در حد توان به عنوان یک مرجع آموزشی سعی میکنیم به اهداف
+                            شغلی خود دست پیدا کنند.
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+
+            <div class="col-md-12 copy-right">
+                تمامی حقوق و مطالب سایت برای وبسافت3 محفوظ می باشد و کپی برداری از مطالب رایگان باذکر منبع آزاد است.
+            </div>
+            <div class="clearfix"></div>
+        </div>
+    </div>
+</footer>
+
+
+<script src="js/jquery-1.11.3.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/main.js"></script>
+
+</body>
+</html>
